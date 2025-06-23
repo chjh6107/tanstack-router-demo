62cf96ed65ca06aa43b8b2e2113aa52a5f922ac7
Jonghun Choe <chjh6107@gmail.com>
2025-06-23 11:05:49 +0900

    style: refactor globals.css for improved theme variables and structure

--- src/routes/children/$id/detail.tsx
+++ src/routes/children/$id/detail.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/children/$id/detail')({
+export const Route = createFileRoute("/children/$id/detail")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/children/$id/detail"!</div>
+  return <div>Hello "/children/$id/detail"!</div>;
 }
